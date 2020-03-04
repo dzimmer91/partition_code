0a9a473591baa9042a51c56bccaedfecb66a9d22
Daniel Zimmerman <dj.zimmerman@fire-eyez.com>
2020-03-03 22:02:27 -0500

    Update File.c

--- src/File.c
+++ src/File.c
@@ -1,5 +1,17 @@
 #include"file_partition.h"
 
+int open_file_desc_rws(char *filename, int *filedesc)
+{
+  *filedesc = open(filename, O_RDWR |  O_SYNC);
+
+  if(*filedesc < 0)
+  {
+    printf("\nError opening file");
+    return -1;
+  }else{
+    return 1; //filedesc;
+  }
+}
 int open_file_desc(char *filename, int *filedesc)
 {
   *filedesc = open(filename, O_RDWR |  O_CREAT);
